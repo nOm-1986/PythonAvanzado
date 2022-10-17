aa164ea25cb20ee03c3ec2e04c5393c773ae9b68
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-10-17 16:47:01 -0500

    Starting advanced course in python

--- 1-OrganizarProyecto.py
+++ 1-OrganizarProyecto.py
@@ -1,5 +1,29 @@
 '''
     Organizar loa archivos de tus proyectos
 
+    Módulo:
+        Un módulo es cualquier archivo de Python. 
+        Generalmente, contien código que puedes reutilizar.
+        - El módulo random para trabajar con la aleatoriedad.
     
+    Paquete:
+        Un paquete es una carpeta que contiene módulos.
+        Siempre posee el archivo __init__.py --> Dunder init- it's short for "double-underscore"
+    
+
+    Ejemplo de organización de archivos en el proyecto: 
+
+        Exploracion_espacial_proyecto
+            - README
+            - .gitignore
+            - venv
+            - exploracion_espacial
+                - __init__.py
+                - nave.py
+                - destino.py
+                - plataforma.py
+                - lanzamiento.py
+                - tests.py
+                - validacion.py
+        
 '''
