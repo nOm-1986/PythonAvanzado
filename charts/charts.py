6d46c5e06982981920ec05eb1ea33cafb05a4505
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-01-28 13:34:33 -0500

    Cambio en gitignore para evitar imagenes y utilizando de nuevo matplotlib para graficar

---
+++ charts/charts.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as pylot
+import random as r
+
+def generate_pie_chart():
+    labels = [x for x in 'ABCDEFG']
+    values = [r.randint(1, 200) for x in range(0, len(labels))]
+    fig, ax = pylot.subplots()
+    ax.pie(values, labels=labels)
+    pylot.savefig('./pie_chart.png')
+    pylot.close()
+
+if __name__ == '__main__':
+    generate_pie_chart()
